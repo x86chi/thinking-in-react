18108e68b7662878906135dc983d93745f010fab
Muhun Kim <iam@muhun.kim>
2020-03-06 18:08:27 +0900

    Write test for SearchBar component

--- src/components/SearchBar.test.tsx
+++ src/components/SearchBar.test.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import { render } from "enzyme";
 import SearchBar from "./SearchBar";
 
+const filterText = "Hello, World!";
+const inStockOnly = false;
+
 describe("SearchBar", () => {
-  const wrapper = render(<SearchBar />);
+  const wrapper = render(
+    <SearchBar filterText={filterText} inStockOnly={inStockOnly} />
+  );
   describe("Input box", () => {
     test("text type input", () => {
       expect(wrapper.find('input[type="text"]').length).toBeGreaterThan(0);
@@ -13,11 +18,18 @@ describe("SearchBar", () => {
         wrapper.find('input[placeholder="Search.."]').length
       ).toBeGreaterThan(0);
     });
+    test(`filterText be '${filterText}'`, () => {
+      expect(wrapper.find('input[type="text"]').val()).toBe(filterText);
+    });
   });
 
   describe("Check box", () => {
+    const checkbox = wrapper.find('input[type="checkbox"]');
     test("check type input", () => {
-      expect(wrapper.find('input[type="checkbox"]').length).toBeGreaterThan(0);
+      expect(checkbox.length).toBeGreaterThan(0);
+    });
+    test("is checked? " + inStockOnly, () => {
+      expect(checkbox.prop("checked")).toBe(inStockOnly);
     });
   });
 
