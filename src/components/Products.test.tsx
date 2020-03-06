33ee05462ba8a26c9b42e9661408d9b3d7883777
Muhun Kim <iam@muhun.kim>
2020-03-06 18:30:53 +0900

    Write test for ProductTable component

--- src/components/Products.test.tsx
+++ src/components/Products.test.tsx
@@ -4,13 +4,64 @@ import { render } from "enzyme";
 import ProductTable, { ProductCategoryRow, ProductRow } from "./Products";
 import { api } from "../api";
 
+const filterText = "";
+const inStockOnly = false;
+
+const stockedCount = api.filter(({ stocked }) => stocked).length;
+
 describe("ProductTable", () => {
-  const rendered = render(<ProductTable products={api} />);
   test("two <th/> element in thead", () => {
-    expect(rendered.find("thead > tr > th").length).toBe(2);
+    expect(
+      render(
+        <ProductTable
+          products={api}
+          filterText={filterText}
+          inStockOnly={inStockOnly}
+        />
+      ).find("thead > tr > th").length
+    ).toBe(2);
   });
-  test("two <th/> element in tbody", () => {
-    expect(rendered.find("tbody > tr > th").length).toBe(2);
+  describe("filter to inStock false", () => {
+    const rendered = render(
+      <ProductTable
+        products={api}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    );
+    test("two <th/> element in tbody", () => {
+      expect(rendered.find("tbody > tr > th").length).toBe(2);
+    });
+    test(`${api.length} Product in tbody`, () => {
+      expect(rendered.find("tbody > tr.product").length).toBe(api.length);
+    });
+  });
+
+  describe("filter to inStock true", () => {
+    const rendered = render(
+      <ProductTable
+        products={api}
+        filterText={filterText}
+        inStockOnly={!inStockOnly}
+      />
+    );
+    test("two <th/> element in tbody", () => {
+      expect(rendered.find("tbody > tr > th").length).toBe(2);
+    });
+    test(`${stockedCount} Product in tbody`, () => {
+      expect(rendered.find("tr.product").length).toBe(stockedCount);
+    });
+  });
+
+  describe(`filterText as ${api[0].name}`, () => {
+    const rendered = render(
+      <ProductTable
+        products={api}
+        filterText={api[0].name}
+        inStockOnly={false}
+      />
+    );
+    expect(rendered.find("tr.product").length).toBe(1);
   });
 });
 
